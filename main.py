22e9df6e429f2f10887a682178d5c2a8ec0f9a74
seba_lara <slara@tecintegral.cl>
2022-11-03 18:08:39 -0300

    Update main code

--- main.py
+++ main.py
@@ -3,39 +3,26 @@ from amqp_handlers import QueueConnection
 import datetime, json, ast
 from mysql.connector import Error, cursor
 
-create_status_table = """CREATE TABLE IF NOT EXISTS`statuses` (
-  `_id` varchar(24) COLLATE 'ascii_general_ci' NOT NULL,
-  `key` varchar(40) COLLATE 'ascii_general_ci' NOT NULL,
-  `status` char(4) COLLATE 'ascii_general_ci' NULL,
-  `instant_status` int(1) NOT NULL,
-  `SED_k` int(1) NOT NULL,
-  `sigma_max` int(1) NOT NULL,
-  `timestamp` date NOT NULL
-) ENGINE='InnoDB';"""
-
-tiempo = datetime.datetime.now()
-timestamp = (tiempo.strftime('%Y-%m-%d %H:%M:%S'))
+now = datetime.datetime.now()
+timestamp = (now.strftime('%Y-%m-%d %H:%M:%S'))
 
 ## SQL Client
-sql_connection = SQLConnection.connection_to_db(
-  '192.168.1.60',3306,'root','tisapolines','polin'
-)
+sql_connection = SQLConnection.connection_to_db('192.168.1.60',3306,'si','tisapolines','polin')
 
 ## AMQP Client
-amqp_obj = QueueConnection()
-amqp_connection = amqp_obj.connection_to_exchange(
+amqp_obj = QueueConnection() 
+amqp_connection = QueueConnection.connection_to_exchange(
   '10.75.10.166', 5672, 'tisa', 'tisa'
 )
 
-
 ## Create table statuses only if not exist
-get_tables = SQLConnection.show_tables(sql_connection)
+show_tables = SQLConnection.show_tables(sql_connection)
 
-if get_tables == None:
-  ('La tabla satuses no existe. \nCreando tabla statuses...')
-  SQLConnection.create_table(sql_connection,create_status_table)
+if show_tables == None:
+  print('No existe la tabla statuses. \nCreando tabla satatuses...')
+  SQLConnection.create_table(sql_connection)
 else:
-  pass
+  print('La tabla statuses existe.')
 
 def callback(ch, method, properties, body):
     
@@ -45,10 +32,10 @@ def callback(ch, method, properties, body):
 
     insert_data_query = """INSERT INTO `statuses` (`_id`,`key`,`status`,`instant_status`,`SED_k`,`sigma_max`,`timestamp`) VALUES ('_id_test',%(key)s,%(status)s,%(instant_status)s,%(SED_k)s,%(sigma_max)s,%(timestamp)s);"""
     SQLConnection.execute_query(sql_connection,insert_data_query,data)
-    print(data)
-    """cursor = sql_connection.cursor()
-    cursor.execute(query,data)
-    sql_connection.commit()"""
+    #print(data)
+    #cursor = sql_connection.cursor()
+    #cursor.execute(query,data)
+    #sql_connection.commit
 
     #sql_connector.execute_query(sql_connection,query,data)
 """for result in cursor.execute(query, multi=True):
